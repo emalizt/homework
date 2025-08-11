0f46a3b4ad454968a6d8f49e08f63e88ea95c5be
emalizt <vlasovrostik9@gmail.com>
2025-08-11 18:00:47 +0300

    add pz 5

---
+++ pz5/max.py
@@ -0,0 +1,6 @@
+def max2(max, min):
+    if max > min:
+        return max
+    else:
+        return min
+print(max2(4, 3))
---
+++ pz5/v4.1.py
@@ -0,0 +1,7 @@
+def task_4_1(N):
+    sum_num = 0
+    for i in range (1, N + 1):
+        sum_num += i
+    return sum_num
+N = 60
+print(task_4_1(N))
