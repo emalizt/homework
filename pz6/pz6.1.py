baa4c56647cdff43ca2adfad35283faf5840ca15
emalizt <vlasovrostik9@gmail.com>
2025-08-22 16:22:22 +0300

    pz6

---
+++ pz6/pz6.1.py
@@ -0,0 +1,30 @@
+# import random
+# c = []
+# i = 0
+# while i < 10:
+#     c.append(random.randint(0,100))
+# i += 1
+# print(c)
+
+
+#сортитовка списка
+# a = [1, 4, 2, 8, 1]
+# a.sort()
+# print(a)
+
+# изменение порядка расположения на обратный
+# a = [1, 3, 5, 7]
+# a.reverse()
+# print(a)
+# Программа запрашивает с клавиатуры пять чисел, добавляет их в список.
+# На экран выводит их сумму, максимальное и минимальное из них
+# ListAppend = []
+# i = 0
+# while i < 5:
+#  ListAppend.append(int(input('Введи значение списка: ')))
+#  i += 1
+# print(ListAppend)
+# print('Сумма элементов списка: ', sum(ListAppend))
+# print('Минимальный элемент списка: ', min(ListAppend))
+# print('Максимальный элемент списка: ', max(ListAppend))
+
